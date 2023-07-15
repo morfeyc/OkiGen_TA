47a92a1c26a61a105854e22782b3fa15e4b18fcf
Morfey <55273349+morfeyc@users.noreply.github.com>
2023-07-15 15:29:29 +0300

    [logic] add fps limit

---
+++ Assets/CodeBase/Utils/TargetFramerate.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace CodeBase.Utils
+{
+  public class TargetFramerate : MonoBehaviour
+  {
+    [SerializeField] private int FPSLimit = 60;
+    
+    private void Awake()
+    {
+      Application.targetFrameRate = FPSLimit;
+    }
+  }
+}
