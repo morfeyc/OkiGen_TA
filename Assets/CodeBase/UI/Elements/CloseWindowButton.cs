c4992f873f51b84f7ddeee380ee0805d5dd534a1
Morfey <55273349+morfeyc@users.noreply.github.com>
2023-07-21 00:25:40 +0300

    Create component to Close window on button

---
+++ Assets/CodeBase/UI/Elements/CloseWindowButton.cs
@@ -0,0 +1,26 @@
+﻿using CodeBase.UI.Windows;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace CodeBase.UI.Elements
+{
+  public class CloseWindowButton : MonoBehaviour
+  {
+    [SerializeField] private Button Button;
+    [SerializeField] private WindowBase Window;
+
+    private void Awake() => 
+      Button.onClick.AddListener(CloseWindow);
+
+    private void OnDestroy() => 
+      Button.onClick.RemoveListener(CloseWindow);
+
+    private void CloseWindow() => 
+      Window.Close();
+
+    private void Reset()
+    {
+      Button = GetComponent<Button>();
+    }
+  }
+}
